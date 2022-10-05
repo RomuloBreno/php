e25bef79691ef3569d3a25dc1df293b6fd3f5cf8
_b.romulo <romulo.breno12@gmail.com>
2022-10-05 01:49:40 -0300

    First commit

--- script.js
+++ script.js
@@ -7,14 +7,10 @@ var h3 = document.querySelector(".nome");
 console.log(btn1)
 
 btn1.addEventListener("click", function(){
-    var up = h3.style.fontSize.value()
-
-    h3.classList.add('font');
 
 });
 
 btn2.addEventListener("click", function(){
-    h3.classList.toggle('font');
 
 });
 });
