22e6941ed542d0533b7349c85f7aa3e136e5d226
_b.romulo <romulo.breno12@gmail.com>
2022-10-06 01:33:12 -0300

    atualização JS

--- phpForm.php
+++ phpForm.php
@@ -1,12 +1,17 @@
 
 <?php
-    if(array_key_exists('nome', $_POST)) {
+    if(array_key_exists('nome', $_POST)) :
         $nome=$_POST["nome"];
         ?>
+
+        
         <h3 class="nome"><?php echo $nome ?></h3> 
-        <button class="btn-1">1</button>
-        <button class="btn-2">2</button>
+        <span>Deseja aumentar a fonte?</span>
+        <button class="btn-1">+</button>
+        <button class="btn-2">-</button>
+        
+        
         <?php
-      }
+      endif;
 
 ?>
--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 
-document.addEventListener("DOMContentLoaded", function (event) {
-var btn1 = document.querySelector(".btn-1");
-var btn2 = document.querySelector(".btn-2");
+document.addEventListener("click", function (event) {
+    var btn1 = document.querySelector(".btn-1");
+    var btn2 = document.querySelector(".btn-2");
 var h3 = document.querySelector(".nome");
+var styleFont = getComputedStyle(h3).fontSize
+var styleFont = styleFont.replace('px','')
+var styleFont = Number(styleFont)
 
-console.log(btn1)
 
-btn1.addEventListener("click", function(){
+btn1.addEventListener("click", upFont);
 
-});
+btn2.addEventListener("click", downFont);
 
-btn2.addEventListener("click", function(){
+function upFont(){
+    console.log(typeof(styleFontNumber))
+    h3.style.fontSize = (styleFont + 1)+'px'
+}
 
-});
+function downFont(){
+    h3.style.fontSize = (styleFont - 1)+'px'
+
+}
+
 });
